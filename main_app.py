eb4532151e4678786992ed644c2811d6c4268e4d
jesusjfy <88951358+jesusjfy@users.noreply.github.com>
2023-07-27 01:15:13 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,7 +8,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/jesusjfy/QAIRA/main/13_Monit
 df.columns = ['Id','Codigo_entidad','Codigo_ubigeo','Codigo_pais','Nombre_uo','Fecha','CO','H2S','NO2','O3','PM10', 'PM25', 'SO2',
        'Ruido', 'UV', 'Humedad', 'Latitud', 'Longitud','Presion', 'Temperatura']
 
-df['Fecha'] = pd.to_datetime(df['Fecha'])
+df['Fecha'] = pd.to_datetime(df['Fecha'], format='ISO8601')
 df = df.sort_values('Fecha')
 
 #df['Fecha_datetime'] = pd.to_datetime(df['Fecha'])
